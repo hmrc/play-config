526b75df59e8b823c62b41f86c0ea70e3d417a45
petrnejedly <pnejedly@equalexperts.com>
2015-03-17 16:12:18 +0000

    updated version to 1.1.0-SNAPSHOT

--- project/HmrcBuild.scala
+++ project/HmrcBuild.scala
@@ -24,7 +24,7 @@ object HmrcBuild extends Build {
   import uk.gov.hmrc.{SbtBuildInfo, ShellPrompt, _}
 
   val appName = "play-config"
-  val appVersion = "1.0.0"
+  val appVersion = "1.1.0-SNAPSHOT"
 
 
   lazy val microservice = Project(appName, file("."))
