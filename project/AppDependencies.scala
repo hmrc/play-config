14c9802c8c8c2b4589908ae505393e3fd2b8e5d0
Dario Abdulrehman <167463+dabd@users.noreply.github.com>
2019-02-14 14:34:49 +0000

    PLATOPS-1850: Library security upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,15 @@ import sbt._
 object AppDependencies {
   val compile = Seq(
     "com.typesafe.play" %% "play"  % PlayVersion.current % "provided",
-    "net.ceedubs"       %% "ficus" % "1.1.1"
+    "net.ceedubs"       %% "ficus" % "1.1.1",
+    // force dependencies due to security flaws found in jackson-databind < 2.9.x using XRay
+    "com.fasterxml.jackson.core"     % "jackson-core"            % "2.9.7",
+    "com.fasterxml.jackson.core"     % "jackson-databind"        % "2.9.7",
+    "com.fasterxml.jackson.core"     % "jackson-annotations"     % "2.9.7",
+    "com.fasterxml.jackson.datatype" % "jackson-datatype-jdk8"   % "2.9.7",
+    "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310" % "2.9.7",
+    // force dependencies due to security flaws found in xercesImpl 2.11.0
+    "xerces" % "xercesImpl" % "2.12.0"
   )
 
   val test = Seq(
